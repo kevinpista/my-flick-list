434442e47e4dfa90690502cebf8da040bf5d9990
kevinpista <120353921+kevinpista@users.noreply.github.com>
2024-01-08 23:04:57 -0800

    Added service function to query TMDB API and add new the movie to local DB when a user creates a watchlist_item

--- backend/services/tmdb_constants.go
+++ backend/services/tmdb_constants.go
@@ -2,5 +2,5 @@ package services
 
 var baseAPIUrl = "https://api.themoviedb.org/3/search/movie?query="
 var baseMovieAPIUrl = "https://api.themoviedb.org/3/movie/"
-var APIKey = "1234"// implement API key via .env load
+var APIKey = "12345"// implement API key via .env load
 
