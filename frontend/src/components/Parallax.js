3c20bc3f2f00ba6800533d7b06e4c9cb3e3b051f
kevinpista <120353921+kevinpista@users.noreply.github.com>
2024-02-06 20:56:02 -0800

    Add hero banner details with button

--- frontend/src/components/Parallax.js
+++ frontend/src/components/Parallax.js
@@ -1,18 +1,26 @@
 import React, { useRef } from "react";
 import { useNavigate } from 'react-router-dom';
-import NavBar from './NavBar.js';
+import Button from '@mui/material/Button';
 import { motion, useScroll, useTransform } from "framer-motion";
 import '../css/Parallax.css';
-import fullImage from '../static/image-full.png';
+// import fullImage from '../static/image-full.png';
 import bottomImage from '../static/image-bottom.png';
 
 import SC7 from '../static/2.png';
 import SC8 from '../static/4.png';
-import SC9 from '../static/4.1.png'; // 4k version 
+import SC10 from '../static/high.png';
+import SC13 from '../static/low4.png';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { muiTheme } from '../css/MuiThemeProvider.js';
 
 
 function Parallax() {
   const navigate = useNavigate();
+
+  const handleGetStartedClick = () => {
+    navigate('/user-registration')
+  };
 
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -25,6 +33,8 @@ function Parallax() {
   // const foregroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
   return (
+    <ThemeProvider theme={muiTheme}>
+
     <div
       ref={ref}
       className="p-container"
@@ -45,7 +55,7 @@ function Parallax() {
           height: "100%",
         }}
       />
-      
+
       <motion.div
         className="full-image "
         style={{
@@ -54,25 +64,43 @@ function Parallax() {
           backgroundSize: "cover",
           y: backgroundY,
         }}
-      />
-
-      <motion.h1 
-        style={{ y: textY }}
-        className="center-header"
       >
-        PARALLAX
-      </motion.h1>
+
+      </motion.div>
+      <motion.div
+          style={{ y: textY }}
+          className="banner-details"
+        >
+          <h1>Curate. Watch. Repeat.</h1>
+          <p>
+            Create endless watchlists of your favorite movies, add notes, and track when you finish watching.
+          </p>
+          <Button
+            variant="contained"
+            onClick={handleGetStartedClick}
+            back="primary"
+            sx={{
+              boxShadow: `0px 0px 2px rgba(255, 255, 255, 1),
+                          0px 0px 6px rgba(255, 255, 255, 0.1),
+                          0px 0px 10px rgba(255, 255, 255, 0.05)`,
+              border: `1px solid rgba(255, 255, 255, 0.25)`,
+            }}
+          >
+            Get Started
+          </Button>
+        </motion.div>
 
       <motion.div
         className="bottom-image"
         style={{
-          backgroundImage: `url(${bottomImage})`,
+          backgroundImage: `url(${SC13})`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
         }}
       />
       
     </div>
+    </ThemeProvider>
   );
 };
 export default Parallax;
