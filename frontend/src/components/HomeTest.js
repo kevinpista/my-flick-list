098a3465ae0f2cbb52614912973d0eb0459a3117
kevinpista <120353921+kevinpista@users.noreply.github.com>
2024-02-11 15:48:01 -0800

    Added numbered steps and linked call to action buttons

--- frontend/src/components/HomeTest.js
+++ frontend/src/components/HomeTest.js
@@ -1,16 +1,7 @@
 // Home.js 
-import { Container, Row, Col } from 'react-bootstrap';
-import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
 import '../css/HomeTest.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './NavBar';
-import MovieSearchBar from './MovieSearchBar.js';
-import Rectangle_Movies from '../static/Rectangle_Movies.jpg';
-import Sqaure_Movies from '../static/Square_Movies.jpg';
-import Square from '../static/Square.jpg';
-import { ThemeProvider } from '@mui/material/styles';
-import { muiTheme } from '../css/MuiThemeProvider.js';
 import Parallax from './Parallax.js';
 
 
@@ -24,11 +15,6 @@ import arrow_right from '../static/arrow-right.png';
 
 
 function HomeTest() {
-  const navigate = useNavigate();
-
-  const handleGetStartedClick = () => {
-    navigate('/user-registration')
-  };
 
   return (
     <div className="home">
@@ -37,26 +23,29 @@ function HomeTest() {
         <div className="features-container">
             <div className="features">
 
-                <div className="first-feature">
+                <div className="feature">
                     <div className="feature-info">
                         <div class="feature-subtitle">
                             Over 850,000 Titles
                         </div>
                         <div class="feature-headline">
-                            Search for Any Movie
+                            Search Any Movie
                         </div>
                         <div class="feature-body">
                             Find nearly every movie that was ever made.
                             Discover spin-offs and highly acclaimed fan made movies of your favorite flicks that you
                             didn't know existed. All powered by 'The Movie Database' (TMDB).
+                            <div class="number-step-bg" id="first-number-step">01</div>
+
                         </div>
                         <div class="feature-info-footer">
-                            <a href="#">
+                            <a href="/movie-search">
                                 <button>
                                     Find a Movie
                                     <img src={arrow_right} />
                                 </button>
                             </a>
+                            
                         </div>   
                     </div>
             
@@ -66,7 +55,7 @@ function HomeTest() {
 
                 </div>
 
-                <div className="first-feature">
+                <div className="feature">
                     <div className="feature-image">
                         <img src={movies_page_img} />
                     </div>
@@ -76,15 +65,16 @@ function HomeTest() {
                             Info On Every Film
                         </div>
                         <div class="feature-headline">
-                            Movie Details & Video Trailer
+                            Movie Details
                         </div>
                         <div class="feature-body">
                             Quickly view in-depth details on any movie all in one place. See user ratings, runtime, 
                             revenue, budget, and its trailer. Add to your watchlists once you decide it's a movie
                             worth watching!
+                            <div class="number-step-bg" id="second-number-step">02</div>
                         </div>
                         <div class="feature-info-footer">
-                            <a href="#">
+                            <a href="/movie-search">
                                 <button>
                                     Try a Search
                                     <img src={arrow_right} />
@@ -94,7 +84,7 @@ function HomeTest() {
                     </div>
                 </div>
 
-                <div className="first-feature">
+                <div className="feature">
                     <div className="feature-info">
                         <div class="feature-subtitle">
                             Take Notes & Check Off
@@ -106,10 +96,11 @@ function HomeTest() {
                             Curate a watchlist and personalize it by mood,
                             genre, actors, or anything that sparks your desire.
                             Add notes to each movie with timestamps.
-                            Stay organized by easily checking off when you finish watching a movie.
+                            Stay organized by easily checking off when you finish a movie.
+                            <div class="number-step-bg" id="first-number-step">03</div>
                         </div>
                         <div class="feature-info-footer">
-                            <a href="#">
+                            <a href="/user-login">
                                 <button>
                                     Personalize
                                     <img src={arrow_right} />
