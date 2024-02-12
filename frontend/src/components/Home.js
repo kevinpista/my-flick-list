4eacab17f6a98f251343a0c2676aa0b45a7d6d79
kevinpista <120353921+kevinpista@users.noreply.github.com>
2024-02-11 18:28:57 -0800

    Home page component update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,106 +1,125 @@
-// Home.js 
-import { Container, Row, Col } from 'react-bootstrap';
-import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
 import '../css/Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './NavBar';
-import MovieSearchBar from './MovieSearchBar.js';
-import Rectangle_Movies from '../static/Rectangle_Movies.jpg';
-import Sqaure_Movies from '../static/Square_Movies.jpg';
-import Square from '../static/Square.jpg';
-import { ThemeProvider } from '@mui/material/styles';
-import { muiTheme } from '../css/MuiThemeProvider.js';
+import NavBar from './NavBar.js';
+import Parallax from './Parallax.js';
+import Footer from './Footer.js';
 
-import SC6 from '../static/first-image.png';
+import movies_page_img from '../static/Movies-Page.png'; // transparent bar
+import SC14 from '../static/Feature5.png'; // white bar
+import SC15 from '../static/Feature6.png'; // yellow bar
+import movies_search_img from '../static/Movie-Search.png';
+import watchlist_img from '../static/Watchlist.png'; 
+import arrow_right from '../static/arrow-right.png'; 
+import tmdb_logo_long from '../static/tmdb-logo-long.svg';
 
 
 function Home() {
-  const navigate = useNavigate();
-
-  const handleGetStartedClick = () => {
-    navigate('/user-registration')
-  };
 
   return (
-    <ThemeProvider theme={muiTheme}>
     <div className="home">
       <NavBar />
-      <div 
-        className="jumbotron"
-        style={{ backgroundImage: `url(${Rectangle_Movies})` }} 
-      >
-        <div className="gradient-overlay">
-          <div className="banner-details">
-          <h1>Curate. Watch. Repeat.</h1>
-          <p>
-            Create endless watchlists of your favorite movies, 
-            add notes, and track when you  finish watching.
-          </p>
-          <Button
-            variant="contained" 
-            onClick={handleGetStartedClick}
-            back="primary" // Use bg instead of color
-            sx={{
-            boxShadow: `0px 0px 2px rgba(255, 255, 255, 1),
-                        0px 0px 6px rgba(255, 255, 255, 0.1),
-                        0px 0px 10px rgba(255, 255, 255, 0.05)`,   
-            border: `1px solid rgba(255, 255, 255, 0.25)`,
-            }}
-          >
-            Get Started
-          </Button>
-          </div>
+      <Parallax />
+        <div className="features-container">
+            <div className="features">
+
+                <div className="feature">
+                    <div className="feature-info">
+                        <div class="feature-subtitle">
+                            Over 850,000 Titles
+                        </div>
+                        <div class="feature-headline">
+                            Search Any Movie
+                        </div>
+                        <div class="feature-body">
+                            Find nearly every movie that was ever made.
+                            Discover spin-offs and highly acclaimed fan made movies of your favorite flicks that you
+                            didn't know existed. All powered by
+                            <span style={{marginLeft: '10px'}}>
+                                <a href="/about">
+                                <img src={tmdb_logo_long } />.
+                                </a>
+                            </span>
+                            <div class="number-step-bg" id="first-number-step">01</div>
+
+                        </div>
+                        <div class="feature-info-footer">
+                            <a href="/movie-search">
+                                <button>
+                                    Find a Movie
+                                    <img src={arrow_right} />
+                                </button>
+                            </a>
+                            
+                        </div>   
+                    </div>
+            
+                    <div className="feature-image">
+                        <img src={movies_search_img} />
+                    </div>
+
+                </div>
+
+                <div className="feature">
+                    <div className="feature-image">
+                        <img src={movies_page_img} />
+                    </div>
+
+                    <div className="feature-info">
+                        <div class="feature-subtitle">
+                            Info On Every Film
+                        </div>
+                        <div class="feature-headline">
+                            Movie Details
+                        </div>
+                        <div class="feature-body">
+                            Quickly view in-depth details on any movie all in one place. See user ratings, runtime, 
+                            revenue, budget, and its trailer. Add to your watchlists once you decide it's a movie
+                            worth watching!
+                            <div class="number-step-bg" id="second-number-step">02</div>
+                        </div>
+                        <div class="feature-info-footer">
+                            <a href="/movie-search">
+                                <button>
+                                    Try a Search
+                                    <img src={arrow_right} />
+                                </button>
+                            </a>
+                        </div>   
+                    </div>
+                </div>
+
+                <div className="feature">
+                    <div className="feature-info">
+                        <div class="feature-subtitle">
+                            Take Notes & Check Off
+                        </div>
+                        <div class="feature-headline">
+                            Create a Watchlist
+                        </div>
+                        <div class="feature-body">
+                            Curate a watchlist and personalize it by mood,
+                            genre, actors, or anything that sparks your desire.
+                            Add notes to each movie with timestamps.
+                            Stay organized by easily checking off when you finish a movie.
+                            <div class="number-step-bg" id="first-number-step">03</div>
+                        </div>
+                        <div class="feature-info-footer">
+                            <a href="/user-login">
+                                <button>
+                                    Personalize
+                                    <img src={arrow_right} />
+                                </button>
+                            </a>
+                        </div>   
+                    </div>
+                    <div className="feature-image">
+                        <img src={watchlist_img} />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-
-      {/* React Bootstrap */}
-      <Container style={{ marginTop: '20px' }}>
-        <MovieSearchBar />
-        <div class="features-headline">
-
-            <h2>Over 850,000 Movies</h2>
-            <p>Search for nearly every movie that was ever made. All powered by 'The Movie Database' (TMDB).</p>
-
-        </div>
-      </Container>
-
-
-
-      <Container class="features">
-      <div class="features-row">
-        <div class="text">
-          <h2>Search for Any Movie</h2>
-          <p>
-            Use the search function powered by TMDB API. See detailed overviews, 
-            release dates, ratings, and finacials for each movie
-          </p>
-        </div>
-        <div class="image"><img src={SC6} alt="movie page"/></div>
-      </div>
-      <div class="features-row">
-      <div class="image"><img src={SC6} alt="list of watchlist page" /></div>
-
-        <div class="text">
-          <h2>Create Your Watchlist</h2>
-          <p>Curate a watchlist and add any movie. Personalize by your mood, genre,
-            favorite director, actors, or anything that sparks you desire to group your movies into 
-            one place.
-             </p>
-        </div>
-      </div>
-      <div class="features-row">
-        <div class="text">
-          <h2>Take Notes & Check Off</h2>
-          <p>Jot down your own notes about each movie before or after you watch. Organize by checkmarking 
-            whenever you've finished watching a movie. 
-          </p>
-        </div>
-        <div class="image"><img src={SC6}  alt="watchlist page" /></div>
-      </div>
-        </Container>
+    <Footer />
     </div>
-    </ThemeProvider>
   );
 }
 
