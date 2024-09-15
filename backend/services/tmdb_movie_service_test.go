37f33229a2079e6effba61f062dd5a541e9273a3
kevinpista <120353921+kevinpista@users.noreply.github.com>
2024-09-14 20:56:56 -0700

    Update .env for local devlopment

--- backend/services/tmdb_movie_service_test.go
+++ backend/services/tmdb_movie_service_test.go
@@ -24,7 +24,7 @@ func TestTMDBGetMovieByID(t *testing.T) {
 func TestTMDBGetMovieByIDAddToLocalDatabase(t *testing.T) {
 	tmdbMovieService := TMDBMovieService{}
 
-	movieIDQuery:= "1791"
+	movieIDQuery:= "1841"
 
 	err := tmdbMovieService.TMDBGetMovieByIDAddToLocalDatabase(movieIDQuery)
 
